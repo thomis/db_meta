bc45085d0251e812b2f903ddbd016dc173eae7d3
Steiner, Thomas <thomas.steiner@novartis.com>
2017-08-29 09:33:32 +0200

    Various
    ! upgrade development dependencies
    ! fix rspec
    ! bump version

--- spec/oracle_spec.rb
+++ spec/oracle_spec.rb
@@ -3,15 +3,13 @@ require 'spec_helper'
 describe DbMeta::Oracle do
 
   it 'has a table type' do
-    object = DbMeta::Oracle::Base.from_type(:table)
+    object = DbMeta::Oracle::Base.from_type('OBJECT_TYPE' => 'TABLE')
     expect(object.class).to eq(DbMeta::Oracle::Table)
   end
 
   it "fails with unknown oracle type" do
-    expect {
-      object = DbMeta::Oracle::Base.from_type(:unknown)
-    }.to raise_error(RuntimeError, 'Oracle type [unknown] is unknown')
-
+    object = DbMeta::Oracle::Base.from_type('OBJECT_TYPE' => 'UNKNOWN')
+    expect(object.class).to eq(DbMeta::Oracle::Base)
   end
 
 end
